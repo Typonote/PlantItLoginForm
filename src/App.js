15cf0c931b366df2b0c3aeac2a090115cd33f9d6
JinKyung Kim <kimradium@naver.com>
2021-11-29 14:00:26 +0900

    #2 2번째 로그인 폼 제작

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Login from "./pages/Login/Login";
+import LoginTwo from "./pages/Login/LoginTwo";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      {/* <Login /> */}
+      <LoginTwo />
     </div>
   );
 }
---
+++ src/pages/Login/LoginTwo.js
@@ -0,0 +1,72 @@
+import React from "react";
+import "./scss/LoginTwo.scss";
+import Logo from "../../Asset/logo.png";
+import GoogleButtonImg from "../../Asset/login_button_google.png";
+import LoginImage from "../../Asset/login.jpeg";
+import NaverButtonImg from "../../Asset/login_button_naver.png";
+
+const LoginTwo = () => {
+  return (
+    <div className="AAA">
+      <div className="BBB">
+        <div className="CCC">
+          <img src={LoginImage} alt="" className="CCC__Image" />
+        </div>
+        <div className="DDD">
+          <img src={Logo} alt="" className="Tlogo__Image" />
+          <a href="###" className="FFF" style={{ textDecoration: "none" }}>
+            <div className="GGG">
+              <img
+                className="HHH"
+                src={GoogleButtonImg}
+                alt="구글 로그인 버튼"
+              />
+            </div>
+            <h1 className="III">구글 로그인</h1>
+          </a>
+
+          <a href="###" className="FFF" style={{ textDecoration: "none" }}>
+            <div className="GGG">
+              <img
+                className="HHH"
+                src={NaverButtonImg}
+                alt="네이버 로그인 버튼"
+              />
+            </div>
+            <h1 className="III">네이버 로그인</h1>
+          </a>
+
+          <div className="JJJ">
+            <span className="KKK"></span>
+            <div className="UUU">플래닛 로그인</div>
+            <span className="KKK"></span>
+          </div>
+          <div className="LLL">
+            <div className="TTT">
+              <label className="MMM">아이디</label>
+            </div>
+            <input className="NNN" type="email" />
+          </div>
+          <div className="LLL">
+            <div className="TTT">
+              <label className="MMM">비밀번호</label>
+            </div>
+            <input className="NNN" type="password" />
+          </div>
+          <div className="OOO">
+            <button className="PPP">로그인</button>
+          </div>
+          <div className="QQQ">
+            <span className="RRR"></span>
+            <a href="###" className="SSS" style={{ textDecoration: "none" }}>
+              회원가입
+            </a>
+            <span className="RRR"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginTwo;
